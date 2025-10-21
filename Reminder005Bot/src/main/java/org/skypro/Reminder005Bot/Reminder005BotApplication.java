2a391ab281bc112f12127878639a6add6b5b28e3
aabdurazakov005 <aabdurazakov005@yandex.ru>
2025-10-21 19:25:19 +0300

    Телеграм-бот для напоминаний

---
+++ Reminder005Bot/src/main/java/org/skypro/Reminder005Bot/Reminder005BotApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.Reminder005Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reminder005BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reminder005BotApplication.class, args);
+	}
+
+}
---
+++ Reminder005Bot/src/test/java/org/skypro/Reminder005Bot/Reminder005BotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.Reminder005Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reminder005BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
